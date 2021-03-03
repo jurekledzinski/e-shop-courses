cf92255c4ce473bba588e0da2b810e718dffa069
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-03-03 21:56:42 +0100

    Add build

---
+++ client/build/npm.webpack.c00dc1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{53:function(n,o){var t=function(){return this}();try{t=t||new Function("return this")()}catch(n){"object"==typeof window&&(t=window)}n.exports=t}}]);
