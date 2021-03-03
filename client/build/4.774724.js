78316dda0f05039c67712fd861d0f289391841e8
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-03-03 16:55:50 +0100

    Remove build

--- client/build/4.774724.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{124:function(n,o,t){"use strict";var c=t(0),a=t.n(c),c=t(143);o.a=function(n){var o=n.history;return a.a.createElement("button",{className:"button-go-back",onClick:function(){return o.goBack()}},"Go back")}},143:function(n,o,t){}}]);
--- client/build/npm.isarray.f2e730.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{88:function(o,r){o.exports=Array.isArray||function(o){return"[object Array]"==Object.prototype.toString.call(o)}}}]);
