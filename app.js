bf737c88166358c82489f21dc2f2190b9f792c76
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-03-03 20:25:24 +0100

    Change sameSite to true in session, change in login form place of media query

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.use(
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 1,
       secure: nodeEnv === "production" ? true : false,
-      sameSite: nodeEnv === "production" ? "none" : "lax",
+      sameSite: nodeEnv === "production" ? true : "lax",
     },
   })
 );
