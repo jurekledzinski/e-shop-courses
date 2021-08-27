a19b07a32712cdb6a58019ad1bc69bff495b1dc0
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-08-27 09:58:35 +0200

    Remove build folder

--- client/build/npm.webpack.c00dc1.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{53:function(n,o){var t=function(){return this}();try{t=t||new Function("return this")()}catch(n){"object"==typeof window&&(t=window)}n.exports=t}}]);
